924c3546907ca30bb448ba557a6abe1327129b7f
Andrew Monks <a@monks.co>
2017-02-03 13:27:53 -0600

    fix demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,50 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-import Component from '../../src'
+import Sortable from '../../src'
 
-let Demo = React.createClass({
-  render() {
-    return <div>
-      <h1>react-sortable Demo</h1>
-      <Component/>
-    </div>
+class Demo extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      collection: [
+        {key: 1, value: 'a'},
+        {key: 2, value: 'b'},
+        {key: 3, value: 'c'},
+        {key: 4, value: 'd'},
+        {key: 5, value: 'e'},
+        {key: 6, value: 'e'}
+      ]
+    }
   }
-})
 
-render(<Demo/>, document.querySelector('#demo'))
+  Item ({value, index, onChange, decorateHandle}) {
+    return (
+      <div>
+        {decorateHandle(<span>+ </span>)}
+        <input
+          onChange={e => {
+            const val = e.target.value
+            onChange(val)
+          }}
+          value={value}
+        />
+      </div>
+    )
+  }
+
+  render () {
+    return (
+      <Sortable
+        collection={this.state.collection}
+        onChange={collection => {
+          this.setState({collection})
+        }}
+        Component={this.Item}
+      />
+    )
+  }
+}
+
+render(<Demo />, document.querySelector('#demo'))
+
